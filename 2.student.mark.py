e08aa4dd5fbf55b965027b5b615818f83941fd4a
MindBr34k3r <nguyenducminh130702@gmail.com>
2022-03-30 08:07:15 +0700

    Put everything into the 2.student.mark.py

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -1,6 +1,116 @@
-from courses import CourseInfo
-from mark import MarkInfo
-from students import StudentInfo
+# from courses import CourseInfo
+# from mark import MarkInfo
+# from students import StudentInfo
+class Course:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class CourseInfo:
+    def __init__(self):
+        self.num = 0
+        self.courses = []
+
+    def num_of_student(self):
+        self.num = int(input("Input number of courses: "))
+        return self.num
+
+    def add_course_info(self):
+        for i in range(self.num_of_student()):
+            id = input("ID: ")
+            name = input("Name: ")
+            course = Course(id, name)
+            self.courses.append(course)
+        print(f"{self.num} courses has been added")
+        return self.courses
+
+    def show_course_info(self):
+        print("{:3} | {:10} | {:20} ".format("No.", "ID", "Name"))
+        for i in range(len(self.courses)):
+            print("{:3} | {:10} | {:20} ".format(str(i + 1), self.courses[i].id, self.courses[i].name))
+
+
+class Mark:
+    def __init__(self, id, name, mark):
+        self.id = id
+        self.name = name
+        self.mark = mark
+
+
+def select_a_course(courses):
+    user_select = input("Input course id: ")
+    for i in range(len(courses)):
+        if user_select == courses[i].id:
+            return courses[i]
+    return "Nah"
+
+
+class MarkInfo:
+    def __init__(self):
+        self.marks = {}
+
+    def add_mark(self, courses, students):
+        select_course = select_a_course(courses)
+        if select_course == "Nah":
+            print("There is no course like that!")
+        else:
+            mark_list = []
+            n = 0
+            for i in range(len(students)):
+                mark = input(f"Student ID: {students[i].id} - Student Name: {students[i].name}\nInput mark: ")
+                student_mark = Mark(students[i].id, students[i].name, mark)
+                mark_list.append(student_mark)
+                n = n + 1
+            self.marks[select_course.id] = mark_list
+            print(f"{n} students has been added")
+            return self.marks
+
+    def show_mark(self, courses, students):
+        select_course = select_a_course(courses)
+        course_mark = self.marks[select_course.id]
+        if select_course == "Nah":
+            print("There is no course like that!")
+        else:
+            print(f"{select_course.name} mark sheet")
+            print("{:3} | {:10} | {:20} | {:10}".format("No.", "ID", "Name", "Mark"))
+            for i in range(len(students)):
+                print("{:3} | {:10} | {:20} | {:10} ".format(str(i + 1), course_mark[i].id, course_mark[i].name,
+                                                             course_mark[i].mark))
+
+
+class Student:
+    def __init__(self, id, name, dob):
+        self.id = id
+        self.name = name
+        self.dob = dob
+
+
+class StudentInfo:
+    def __init__(self):
+        self.num = 0
+        self.students = []
+
+    def num_of_student(self):
+        self.num = int(input("Input number of students: "))
+        return self.num
+
+    def add_student_info(self):
+        for i in range(self.num_of_student()):
+            id = input("ID: ")
+            name = input("Name: ")
+            dob = input("Date of Birth: ")
+            student = Student(id, name, dob)
+            self.students.append(student)
+        print(f"{self.num} students has been added")
+        return self.students
+
+    def show_student_info(self):
+        print("{:3} | {:10} | {:20} | {:20}".format("No.", "ID", "Name", "Date of Birth"))
+        for i in range(len(self.students)):
+            print("{:3} | {:10} | {:20} | {:20}".format(str(i + 1), self.students[i].id, self.students[i].name,
+                                                        self.students[i].dob))
+
 
 end = False
 student_info = StudentInfo()
